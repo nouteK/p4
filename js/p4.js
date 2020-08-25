b6ee31065c13b24ef05abf07ffceff1b7dbe5934
nouteK <trofimiuk.pawel@gmail.com>
2020-08-25 14:29:17 +0200

    jeu operationnel, manque des animations et custom css

--- js/p4.js
+++ js/p4.js
@@ -3,9 +3,11 @@ class P4{
 		this.COL = 7;
 		this.LGN = 6;
 		this.selector = selector;
+		this.player = 'red';
 
 		this.drawGame();
 		this.ecoute();
+		this.checkWin();
 	}
 
 	//Affichage du jeu
@@ -40,19 +42,81 @@ class P4{
 			const $col = $(this).data('col');
 			const $last = lastCase($col);
 			if ($last != null) {
-				$last.addClass('pred');
+				$last.addClass(`p${that.player}`);
 			}
 		});
 
 		$jeu.on('mouseleave', '.col', function(){
-			$('.col').removeClass('pred');
+			$('.col').removeClass(`p${that.player}`);
 		});
 
 		$jeu.on('click', '.col.empty', function(){
 			const col = $(this).data('col');
 			const $last = lastCase(col);
-			$last.addClass('red').removeClass('empty pred').data('player', 'red');
+			$last.addClass(`${that.player}`).removeClass(`empty p${that.player}`).data('player', `${that.player}`);
 			
+			const winner = that.checkWin($last.data('lgn'), $last.data('col'));
+
+			that.player = (that.player === 'red') ? 'yellow' : 'red';
+
+			if(winner){
+				console.log(`Les ${winner} ont win`);
+				$('#restart').css('visibility', "visible");
+				return;
+			}
+
 		});
+	}
+
+	checkWin(lgn, col){
+		const that = this;
+
+		function $getCell(i, j){
+			return $(`.col[data-lgn='${i}'][data-col='${j}']`);
+		}
+
+		function checkDirection(direction){
+			let total = 0;
+			let i = lgn + direction.i;
+			let j = col + direction.j;
+			let $next = $getCell(i, j);
+
+			while(i >= 0 && i < that.LGN && j >= 0 && j < that.COL && $next.data('player') == that.player){
+				total++;
+				i += direction.i;
+				j += direction.j;
+				$next = $getCell(i, j);
+
+			}
+			return total;
+		}
+
+		function checkWin(directionA, directionB){
+			const total = 1 + checkDirection(directionA) + checkDirection(directionB);
+			if (total>=4){
+				return that.player;
+			 }
+			else{
+				return null;
+			}
+		}
+
+		function checkHori(){
+			return checkWin({i: 0, j: -1}, {i: 0, j: 1});
+		}
+
+		function checkVerti(){
+			return checkWin({i: -1, j: 0}, {i: 1, j: 0});
+		}
+
+		function checkDiag1(){
+			return checkWin({i: 1, j: 1}, {i: -1, j: -1});
+		}
+
+		function checkDiag2(){
+			return checkWin({i: 1, j: -1}, {i: -1, j: 1});
+		}
+
+		return checkHori() || checkVerti() || checkDiag1() || checkDiag2();
 	}
 }
